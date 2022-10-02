e34ad8389d2a16277e741225d377cce4cc67f10c
romaisaev <roma.isaev.001@mail.ru>
2022-10-02 03:04:39 +0300

    Initial Commit

---
+++ WB_adversiting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WB_adversiting
+//
+//  Created by Рома Исаев on 02.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
